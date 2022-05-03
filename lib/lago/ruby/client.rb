39be6cb734ff12879a34b39524e2b439bd5e8c33
Lovro Colic <lovro.colic.lc@gmail.com>
2022-05-03 13:33:49 +0200

    Initialize repo

---
+++ lib/lago/ruby/client.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "client/version"
+
+module Lago
+  module Ruby
+    module Client
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/lago/ruby/client/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Lago
+  module Ruby
+    module Client
+      VERSION = "0.1.0"
+    end
+  end
+end
