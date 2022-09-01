ea60152f9f014be0bffd2ef95ab29bbfc3c1792c
LovroColic <lovro.colic.lc@gmail.com>
2022-09-01 15:35:41 +0200

    fix: Fix several data types in readme (#37)
    * fix data types in readme
    
    * fix gemfile.lock

--- spec/factories/wallet_transaction.rb
+++ spec/factories/wallet_transaction.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :wallet_transaction, class: OpenStruct do
     wallet_id { '123' }
-    paid_credits { 100 }
-    granted_credits { 100 }
+    paid_credits { '100' }
+    granted_credits { '100' }
   end
 end
