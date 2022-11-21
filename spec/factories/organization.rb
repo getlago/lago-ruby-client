28ef314676df3ca0baf8b5aa72e9f9156a28e028
Romain Sempé <romain.sempe@qonto.eu>
2022-11-21 11:45:16 +0100

    misc: Move fields to billing configuration

--- spec/factories/organization.rb
+++ spec/factories/organization.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :organization, class: OpenStruct do
     webhook_url { 'http://example.com/webhooks/' }
-    vat_rate { 20 }
     country { 'country' }
     address_line1 { 'line1' }
     address_line2 { 'line2' }
@@ -11,6 +10,11 @@ FactoryBot.define do
     city { 'city' }
     legal_name { 'legal1' }
     legal_number { 'legal2' }
-    invoice_footer { 'footer' }
+    billing_configuration do
+      {
+        invoice_footer: 'footer',
+        vat_rate: 20,
+      }
+    end
   end
 end
