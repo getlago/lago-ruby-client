f4d599899d904c89c4d607bffd6da5674072d767
LovroColic <lovro.colic.lc@gmail.com>
2023-01-17 13:15:27 +0100

    feat (invoices): add support for invoice retry payment (#61)

--- lib/lago/api/resources/invoice.rb
+++ lib/lago/api/resources/invoice.rb
@@ -33,6 +33,13 @@ module Lago
           JSON.parse(response.to_json, object_class: OpenStruct).invoice
         end
 
+        def retry_payment(invoice_id)
+          path = "/api/v1/invoices/#{invoice_id}/retry_payment"
+          response = connection.post({}, path)
+
+          JSON.parse(response.to_json, object_class: OpenStruct)
+        end
+
         def whitelist_params(params)
           {
             root_name => {
