d5b46463d718232d4dfbd025879034591c7ae3bc
LovroColic <lovro.colic.lc@gmail.com>
2023-02-07 11:27:27 +0100

    feat (coupons): add support for coupons that have plan limitations (#70)

--- lib/lago/api/resources/coupon.rb
+++ lib/lago/api/resources/coupon.rb
@@ -13,21 +13,29 @@ module Lago
         end
 
         def whitelist_params(params)
-          {
-            root_name => {
-              name: params[:name],
-              code: params[:code],
-              amount_cents: params[:amount_cents],
-              amount_currency: params[:amount_currency],
-              percentage_rate: params[:percentage_rate],
-              coupon_type: params[:coupon_type],
-              reusable: params[:reusable],
-              frequency: params[:frequency],
-              frequency_duration: params[:frequency_duration],
-              expiration: params[:expiration],
-              expiration_at: params[:expiration_at],
-            }.compact,
-          }
+          result_hash = {
+            name: params[:name],
+            code: params[:code],
+            amount_cents: params[:amount_cents],
+            amount_currency: params[:amount_currency],
+            percentage_rate: params[:percentage_rate],
+            coupon_type: params[:coupon_type],
+            reusable: params[:reusable],
+            frequency: params[:frequency],
+            frequency_duration: params[:frequency_duration],
+            expiration: params[:expiration],
+            expiration_at: params[:expiration_at],
+          }.compact
+
+          whitelist_limitations(params[:applies_to]).tap do |limitations|
+            result_hash[:applies_to] = limitations unless limitations.empty?
+          end
+
+          { root_name => result_hash }
+        end
+
+        def whitelist_limitations(limitation_params)
+          (limitation_params || {}).slice(:plan_codes)
         end
       end
     end
