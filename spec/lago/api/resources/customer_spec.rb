9babb47d59297e5b4420e006009cc1d9b7ea3f9e
LovroColic <lovro.colic.lc@gmail.com>
2023-03-28 15:07:46 +0200

    feat(customer-portal): add support for customer portal url (#85)

--- spec/lago/api/resources/customer_spec.rb
+++ spec/lago/api/resources/customer_spec.rb
@@ -129,6 +129,41 @@ RSpec.describe Lago::Api::Resources::Customer do
     end
   end
 
+  describe '#plan_url' do
+    context 'when the customer exists' do
+      before do
+        url_json = JSON.generate(
+          'customer' => {
+            'portal_url' =>
+              'https://app.lago.dev/customer-portal/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaEpJaWt3WkdGbE1qWmx'\
+              'ZUzFqWlRnekxUUTJZell0T1dRNFl5MHdabVF4TURabFlqY3dNVElHT2daRlZBPT0iLCJleHAiOiIyMDIzLTAzLTIzVDIzOjAzOjAwL'\
+              'jM2NloiLCJwdXIiOm51bGx9fQ==--7128c6e541adc7b4c14249b1b18509f92e652d17'
+          }
+        )
+
+        stub_request(:get, 'https://api.getlago.com/api/v1/customers/external_customer_id/portal_url')
+          .to_return(body: url_json, status: 200)
+      end
+
+      it 'returns the customer portal url' do
+        portal_url = resource.portal_url('external_customer_id')
+
+        expect(portal_url).to include('https://app.lago.dev/customer-portal/')
+      end
+    end
+
+    context 'when the customer does not exists' do
+      before do
+        stub_request(:get, 'https://api.getlago.com/api/v1/customers/DOESNOTEXIST/portal_url')
+          .to_return(body: JSON.generate(status: 404, error: 'Not Found'), status: 404)
+      end
+
+      it 'raises an error' do
+        expect { resource.portal_url('DOESNOTEXIST') }.to raise_error Lago::Api::HttpError
+      end
+    end
+  end
+
   describe '#destroy' do
     context 'when customer is successfully destroyed' do
       before do
