2ba1e96ad37d014e81ab9a4da63c70887e71f77c
Romain Sempé <romain@getlago.com>
2023-03-30 14:08:57 +0200

    feat(instant_charge): Ability to have an instant charge (#77)

--- spec/factories/plan.rb
+++ spec/factories/plan.rb
@@ -16,6 +16,7 @@ FactoryBot.define do
         {
           billable_metric_id: 'id',
           charge_model: 'standard',
+          instant: false,
           properties: { amount: '0.22' },
         },
       ]
