b891c3ec35d95937b938e08d0d28e950f0ef0434
Romain Sempé <romain@getlago.com>
2023-08-09 08:55:30 +0200

    feat(tax): Add tax_codes to one-off invoice creation

--- lib/lago/api/resources/invoice.rb
+++ lib/lago/api/resources/invoice.rb
@@ -90,7 +90,7 @@ module Lago
           processed_fees = []
 
           fees.each do |f|
-            result = (f || {}).slice(:add_on_code, :unit_amount_cents, :units, :description)
+            result = (f || {}).slice(:add_on_code, :unit_amount_cents, :units, :description, :tax_codes)
 
             processed_fees << result unless result.empty?
           end
--- spec/factories/tax.rb
+++ spec/factories/tax.rb
@@ -2,10 +2,10 @@
 
 FactoryBot.define do
   factory :create_tax, class: OpenStruct do
-    name { 'name_rate' }
-    code { 'code_rate' }
+    name { 'name_tax' }
+    code { 'code_tax' }
     rate { 15.0 }
-    description { 'description_rate' }
+    description { 'description_tax' }
     applied_to_organization { false }
   end
 end
