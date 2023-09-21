4584140b996c74c0f0cf9dd95a27205202b91819
LovroColic <lovro.colic.lc@gmail.com>
2023-09-21 17:01:24 +0200

    add support for subscription ending_at (#142)

--- spec/factories/subscription.rb
+++ spec/factories/subscription.rb
@@ -8,6 +8,7 @@ FactoryBot.define do
     status { 'active' }
     billing_time { 'calendar' }
     started_at { '2022-05-05T12:27:30Z' }
+    ending_at { '2022-08-05T00:00:00Z' }
     subscription_date { '2022-05-05' }
     terminated_at { nil }
     canceled_at { nil }
