a9527c052934305f09adde4f448337d327ee7af8
Ivan Novosad <ivannovosad@users.noreply.github.com>
2023-11-09 09:43:04 +0100

    feat(default-currency): Add organization default currency (#152)
    * feat(default-currency): Add organization default currency
    
    * Add invoice display name to customer usage

--- spec/factories/organization.rb
+++ spec/factories/organization.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     webhook_url { 'http://example.com/webhooks/' }
     webhook_urls { ['http://example.com/webhooks/', 'http://example2.com/webhooks/'] }
     country { 'country' }
+    default_currency { 'USD' }
     address_line1 { 'line1' }
     address_line2 { 'line2' }
     state { 'state' }
