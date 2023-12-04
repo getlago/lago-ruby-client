2863f88bc6ca621e97c8cd57dbbbf4712ce7b855
Lovro Colic <lovro.colic.lc@gmail.com>
2023-12-04 17:29:53 +0100

    fix tests

--- spec/factories/organization.rb
+++ spec/factories/organization.rb
@@ -16,7 +16,7 @@ FactoryBot.define do
     legal_number { 'legal2' }
     net_payment_term { 0 }
     tax_identification_number { 'EU123456789' }
-    document_numbering { 1 }
+    document_numbering { 'per_customer' }
     document_number_prefix { 'ORG-1234' }
     billing_configuration do
       {
