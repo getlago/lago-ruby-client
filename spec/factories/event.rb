b26ba709818a10b4a27d5e290d868631faa3940d
Jérémy Denquin <jdenquin@users.noreply.github.com>
2024-01-17 11:22:57 +0100

    feat(events): Add Batch events endpoint (#169)
    * feat(events): Add Batch events endpoint
    
    * fix specs

--- spec/factories/event.rb
+++ spec/factories/event.rb
@@ -12,14 +12,18 @@ FactoryBot.define do
   end
 
   factory :batch_event, class: OpenStruct do
-    transaction_id { 'UNIQUE_ID' }
-    external_subscription_ids { %w[5eb02857-a71e-4ea2-bcf9-57d8885990ba 5eb02857-a71e-4ea2-bcf9-57d8885990ba] }
-    code { '123' }
-    timestamp { '2022-05-05T12:27:30Z' }
-    properties do
-      {
-        'custom_field' => 'custom'
-      }
+    events do
+      [
+        {
+          'transaction_id' => 'UNIQUE_ID',
+          'external_subscription_id' => '5eb02857-a71e-4ea2-bcf9-57d8885990ba',
+          'code' => '123',
+          'timestamp' => '2022-05-05T12:27:30Z',
+          'properties' => {
+            'custom_field' => 'custom',
+          },
+        },
+      ]
     end
   end
 
