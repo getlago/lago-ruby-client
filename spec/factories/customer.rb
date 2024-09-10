bf0ae58a4df2472601cfe4e420c3879205bc7fdd
Miguel Pinto <darkymiguel@gmail.com>
2024-09-10 18:11:35 +0100

    feat: rubocop

--- spec/factories/customer.rb
+++ spec/factories/customer.rb
@@ -4,6 +4,9 @@ FactoryBot.define do
   factory :create_customer, class: OpenStruct do
     external_id { '1a901a90-1a90-1a90-1a90-1a901a901a90' }
     name { 'Gavin Belson' }
+    firstname { 'Gavin' }
+    lastname { 'Belson' }
+    customer_type { 'individual' }
     country { 'US' }
     address_line1 { '5230 Penfield Ave' }
     address_line2 { 'fzufuzfuz' }
