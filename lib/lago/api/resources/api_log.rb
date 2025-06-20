ccce65e354455dce1236bb6a878e0aecac8878c1
Mario Diniz <mariohenriquediniz@gmail.com>
2025-06-20 12:56:23 -0300

    feat(api-logs):api logs endpoints

---
+++ lib/lago/api/resources/api_log.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Lago
+  module Api
+    module Resources
+      class ApiLog < Base
+        undef_method :create, :update, :destroy
+
+        def api_resource
+          'api_logs'
+        end
+
+        def root_name
+          'api_log'
+        end
+
+        def whitelist_params(params)
+          {
+            root_name => {
+              api_log: params[:request_id],
+            },
+          }
+        end
+      end
+    end
+  end
+end
